f0496891c65853405416910054f893ca484c00ff
hakant <hakantuncer@gmail.com>
2016-01-17 18:50:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+export class App{
+  constructor(){
+    this.message = "";
+  }
+
+  activate(){
+    this.message = "Hello from Aurelia!";
+  }
+
+  changeMessage(){
+    this.message = "Goodbye!";
+  }
+}
